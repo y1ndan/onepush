08d5e54dffec11caa9e197df6b0d9b4627c53b63
y1ndan <i@yindan.me>
2021-10-30 23:09:34 +0800

    style: minor improvement

--- onepush/providers/gocqhttp.py
+++ onepush/providers/gocqhttp.py
@@ -10,13 +10,17 @@ from ..core import Provider
 class Gocqhttp(Provider):
     name = 'gocqhttp'
     base_url = 'http://{}:{}/send_private_msg'
+    site_url = 'https://docs.go-cqhttp.org'
 
     _params = {
         'required': ['ip_domain', 'port', 'qq'],
         'optional': ['title', 'content']
     }
 
-    def _prepare_url(self, ip_domain: str = '127.0.0.1', port: int = 5700, **kwargs):
+    def _prepare_url(self,
+                     ip_domain: str,
+                     port: int,
+                     **kwargs):
         self.url = self.base_url.format(ip_domain, port)
         return self.url
 
@@ -26,6 +30,6 @@ class Gocqhttp(Provider):
                       content: str = None,
                       **kwargs):
         message = self.process_message(title, content)
-        self.data = {'user_id':qq, 'message': message}
+        self.data = {'user_id': qq, 'message': message}
         return self.data
-      
+
