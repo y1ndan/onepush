f24f9866129cf9052ffca8855e69846872613ca6
qq57240 <xiaopan@qq.com>
2021-10-30 22:21:40 +0800

    Update __init__.py

--- onepush/providers/__init__.py
+++ onepush/providers/__init__.py
@@ -6,6 +6,7 @@
 
 from . import bark
 from . import custom
+from . import gocqhttp
 from . import dingtalk
 from . import discord
 from . import pushplus
@@ -19,6 +20,7 @@ from . import wechatworkbot
 _all_providers = {
     bark.Bark.name: bark.Bark,
     custom.Custom.name: custom.Custom,
+    gocqhttp.Gocqhttp.name: gocqhttp.Gocqhttp,
     dingtalk.DingTalk.name: dingtalk.DingTalk,
     discord.Discord.name: discord.Discord,
     pushplus.PushPlus.name: pushplus.PushPlus,
