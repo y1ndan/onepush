295e9e47edec5f4c938bb96b0d12a34b72bcf759
y1ndan <i@yindan.me>
2021-11-01 12:42:20 +0800

    feature: gocqhttp supports more parameters

--- onepush/providers/gocqhttp.py
+++ onepush/providers/gocqhttp.py
@@ -9,27 +9,46 @@ from ..core import Provider
 
 class Gocqhttp(Provider):
     name = 'gocqhttp'
-    base_url = 'http://{}:{}/send_private_msg'
     site_url = 'https://docs.go-cqhttp.org'
 
     _params = {
-        'required': ['ip_domain', 'port', 'qq'],
-        'optional': ['title', 'content']
+        'required': ['endpoint'],
+        'optional': [
+            'title', 'content', 'path', 'token', 'message_type', 'user_id',
+            'group_id', 'auto_escape'
+        ]
     }
 
-    def _prepare_url(self,
-                     ip_domain: str,
-                     port: int,
-                     **kwargs):
-        self.url = self.base_url.format(ip_domain, port)
+    def __init__(self):
+        super().__init__()
+        self.method = 'get'
+
+    def _prepare_url(self, endpoint: str, path: str = None, **kwargs):
+        if endpoint and 'http' not in endpoint:
+            endpoint = f'http://{endpoint}'
+        if not path:
+            path = '/send_msg'
+
+        self.url = f'{endpoint}{path}'
         return self.url
 
     def _prepare_data(self,
-                      qq: int,
                       title: str = None,
                       content: str = None,
+                      token: str = None,
+                      message_type: str = None,
+                      user_id: int = None,
+                      group_id: int = None,
+                      auto_escape: bool = False,
                       **kwargs):
         message = self.process_message(title, content)
-        self.data = {'user_id': qq, 'message': message}
+        self.data = {
+            'access_token': token,
+            'message_type': message_type,
+            'user_id': user_id,
+            'group_id': group_id,
+            'message': message,
+            'auto_escape': auto_escape
+        }
         return self.data
 
